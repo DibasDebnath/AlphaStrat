0ba589f28db011947d5e6374f90ad416b449f1a2
Dibas Debnath <38382371+DibasDebnath@users.noreply.github.com>
2020-12-05 20:51:20 -0500

    Camera CLamp

--- Assets/Scripts/Camera/CameraTargetController.cs
+++ Assets/Scripts/Camera/CameraTargetController.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class CameraTargetController : MonoBehaviour
 {
     public float movementMultiplier;
+
+    [Header("Target Locker")]
+    public float Up;
+    public float Down;
+    public float Left;
+    public float Right;
+
+
+    private Vector3 oldPos;
+
     // Start is called before the first frame update
     private void Update()
     {
-        this.transform.Translate(new Vector3(RefHolder.instance.inputController.MoveDisplacementX * movementMultiplier, 0,RefHolder.instance.inputController.MoveDisplacementY * movementMultiplier));
+        if(this.transform.position.x >= Left && this.transform.position.x <= Right && this.transform.position.z <= Up && this.transform.position.z >= Down)
+        {
+            oldPos = this.transform.position;
+            this.transform.Translate(new Vector3(RefHolder.instance.inputController.MoveDisplacementX * movementMultiplier, 0, RefHolder.instance.inputController.MoveDisplacementY * movementMultiplier));
+        }
+        else
+        {
+            this.transform.position = oldPos;
+        }
+        
     }
 }
